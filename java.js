9b5e17b9845166093705bd24048af22b5b26ab64
Isa Marques <80493617+isamsm@users.noreply.github.com>
2022-05-16 00:13:41 -0300

    Update java.js

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
 document.write("<table border='1' align='center'>")
 
 for (let x = 10; x<= 20; x++) {
-    document.write("<tr class='linha"+x+"'>") //tr é coluna, x. aqui é definida a classe de linha + o número dela
+    document.write("<tr>") //tr é coluna, x. aqui é definida a classe de linha + o número dela
         for (let y = 1; y<= 10; y++) {
             document.write("<td>") 
             document.write(y + ' * ' + x + ' = ' + (x * y))
